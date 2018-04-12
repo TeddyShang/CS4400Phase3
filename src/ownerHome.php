4419fee10962608a69adbeaaf0c6f4d307284d3b
TeddyShang <teddyshang99@gmail.com>
2018-04-12 10:34:59 -0400

    Added sessions. ownerHome has basic functionality now.

--- src/ownerHome.php
+++ src/ownerHome.php
@@ -1,16 +1,62 @@
+<?php
+session_start();
+require_once 'config.php';
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-    <title>OwnerHome</title>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.css">
 </head>
 <body>
-<h1>Welcome</h1>
-<h2>Your Properties</h2>
+<h1>Welcome <?php echo $_SESSION['userID']?></h1>
+<h1>Your Properties:</h1>
+<table id="example" class="display">
+    <thead>
+    <tr>
+        <th>Name</th>
+        <th>Street</th>
+        <th>City</th>
+        <th>Zip</th>
+        <th>Size</th>
+        <th>PropertyType</th>
+        <th>isPublic</th>
+        <th>isCommerical</th>
+        <th>ID</th>
+    </tr>
+    </thead>
+    <tbody>
+<?php
+$ownerName= $_SESSION['userID'];
+$sql = "SELECT Name, Street, City, Zip, Size, PropertyType, isPublic, isCommercial, ID FROM Property WHERE Owner = '$ownerName'";
+$result = $conn->query($sql);
+
+while($row = mysqli_fetch_array($result)) {
+    echo "<tr>";
+    echo "<td>" .$row['Name'] . "</td>";
+    echo "<td>" .$row['Street'] . "</td>";
+    echo "<td>" .$row['City'] . "</td>";
+    echo "<td>" .$row['Zip'] . "</td>";
+    echo "<td>" .$row['Size'] . "</td>";
+    echo "<td>" .$row['PropertyType'] . "</td>";
+    echo "<td>" .$row['isPublic'] . "</td>";
+    echo "<td>" .$row['isCommercial'] . "</td>";
+    echo "<td>" .$row['ID'] . "</td>";
+    echo "</tr>";
+}
+
+
+$conn ->close();
+?>
+    </tbody>
+</table>;
+
+<script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.2.min.js"></script>
+<script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/jquery.dataTables.min.js"></script>
+<script>
+    $(function(){
+        $("#example").dataTable();
+    })
+</script>
 </body>
+</html>
--- src/validateUser.php
+++ src/validateUser.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 // Include config file
 require_once 'config.php';
 if ( isset( $_POST['submit'] ) ) {
@@ -7,13 +8,14 @@ if ( isset( $_POST['submit'] ) ) {
     $password = md5($password);
     $success = "Matching in DB";
     $failure = "The username/password combination was incorrect";
-    $sql = "SELECT Email, Password, UserType From User Where (Email='$username' AND Password='$password')";
+    $sql = "SELECT Username, Email, Password, UserType From User Where (Email='$username' AND Password='$password')";
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
         $row = mysqli_fetch_row($result);
-        if ($row[2] == "ADMIN") {
+        $_SESSION["userID"] = $row[0];
+        if ($row[3] == "ADMIN") {
             echo "<script>window.location = 'adminHome.php'</script>";
-        } elseif($row[2] == "VISITOR") {
+        } elseif($row[3] == "VISITOR") {
             echo "<script>window.location = 'visitorHome.php'</script>";
         } else {
             echo "<script>window.location = 'ownerHome.php'</script>";
