fb1b3ae0b91eda85c370b16fe517481e2698f018
TeddyShang <teddyshang99@gmail.com>
2018-04-12 11:17:02 -0400

    Search functionality added to all columns. Added missing columns to ownerHome.

--- src/ownerHome.php
+++ src/ownerHome.php
@@ -11,7 +11,7 @@ require_once 'config.php';
 <body>
 <h1>Welcome <?php echo $_SESSION['userID']?></h1>
 <h1>Your Properties:</h1>
-<table id="example" class="display">
+<table id="ownerProperties" class="display">
     <thead>
     <tr>
         <th>Name</th>
@@ -23,15 +23,47 @@ require_once 'config.php';
         <th>isPublic</th>
         <th>isCommerical</th>
         <th>ID</th>
+        <th>isValid</th>
+        <th>Visits</th>
+        <th>Average Rating</th>
     </tr>
     </thead>
     <tbody>
 <?php
 $ownerName= $_SESSION['userID'];
-$sql = "SELECT Name, Street, City, Zip, Size, PropertyType, isPublic, isCommercial, ID FROM Property WHERE Owner = '$ownerName'";
+$sql = "SELECT Name, Street, City, Zip, Size, PropertyType, isPublic, isCommercial, ID, ApprovedBy FROM Property WHERE Owner = '$ownerName'";
 $result = $conn->query($sql);
 
 while($row = mysqli_fetch_array($result)) {
+    $publicBool;
+    $commercialBool;
+    $id = $row['ID'];
+    $idDigits = sprintf('%05d', $row['ID']);
+    if ($row['isPublic'] == 1) {
+        $publicBool = "True";
+    } else {
+        $publicBool = "False";
+    }
+    if ($row['isCommercial'] == 1) {
+        $commercialBool = "True";
+    } else {
+        $commercialBool = "False";
+    }
+    $isValid;
+    if ($row['ApprovedBy'] == "NULL") {
+        $isValid = "False";
+    } else {
+        $isValid = "True";
+    }
+    $visitorStatsSQL = "SELECT COUNT(PropertyID), AVG(Rating) FROM Visit WHERE PropertyID = '$id'";
+    $statsResult = $conn->query($visitorStatsSQL);
+    $stats = mysqli_fetch_array($statsResult);
+    $visits = $stats[0];
+    $rating = $stats[1];
+    if (!($rating>0)) {
+        $rating = "N/A";
+    }
+
     echo "<tr>";
     echo "<td>" .$row['Name'] . "</td>";
     echo "<td>" .$row['Street'] . "</td>";
@@ -39,9 +71,12 @@ while($row = mysqli_fetch_array($result)) {
     echo "<td>" .$row['Zip'] . "</td>";
     echo "<td>" .$row['Size'] . "</td>";
     echo "<td>" .$row['PropertyType'] . "</td>";
-    echo "<td>" .$row['isPublic'] . "</td>";
-    echo "<td>" .$row['isCommercial'] . "</td>";
-    echo "<td>" .$row['ID'] . "</td>";
+    echo "<td> $publicBool </td>";
+    echo "<td> $commercialBool</td>";
+    echo "<td>$idDigits</td>";
+    echo "<td>$isValid</td>";
+    echo "<td>$visits</td>";
+    echo "<td>$rating</td>";
     echo "</tr>";
 }
 
@@ -49,14 +84,51 @@ while($row = mysqli_fetch_array($result)) {
 $conn ->close();
 ?>
     </tbody>
+    <tfoot>
+    <tr>
+        <th>Name</th>
+        <th>Street</th>
+        <th>City</th>
+        <th>Zip</th>
+        <th>Size</th>
+        <th>PropertyType</th>
+        <th>isPublic</th>
+        <th>isCommerical</th>
+        <th>ID</th>
+        <th>isValid</th>
+        <th>Visits</th>
+        <th>Average Rating</th>
+    </tr>
+
+    </tfoot>
 </table>;
 
-<script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.2.min.js"></script>
-<script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/jquery.dataTables.min.js"></script>
+<script type="text/javascript" charset="utf8" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.js"></script>
 <script>
-    $(function(){
-        $("#example").dataTable();
-    })
+    $(document).ready(function() {
+        // Setup - add a text input to each footer cell
+        $('#ownerProperties tfoot th').each( function () {
+            var title = $(this).text();
+            $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+        } );
+
+        // DataTable
+        var table = $('#ownerProperties').DataTable();
+
+        // Apply the search
+        table.columns().every( function () {
+            var that = this;
+
+            $( 'input', this.footer() ).on( 'keyup change', function () {
+                if ( that.search() !== this.value ) {
+                    that
+                        .search( this.value )
+                        .draw();
+                }
+            } );
+        } );
+    } );
 </script>
 </body>
 </html>
