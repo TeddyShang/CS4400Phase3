7793e3282b25b78f040df3fe66aed6992bdb5d63
TeddyShang <teddyshang99@gmail.com>
2018-04-17 21:07:50 -0400

    Admin can now delete users

---
+++ src/deleteUser.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+require_once 'config.php';
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+</head>
+<body>
+<?php
+$username = $_POST["others"];
+$message = "User removed from database";
+$sql = "DELETE FROM User Where Username = '$username'";
+$conn ->query($sql);
+echo "<script type='text/javascript'>alert('$message');</script>";
+echo "<script>window.location = 'adminHome.php'</script>";
+?>
